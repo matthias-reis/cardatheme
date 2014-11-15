602ec45929ce1f3746538d5071b56353821ebfea
mre <matthias.reis@gmail.com>
2014-11-15 18:19:54 +0100

    Total reset

--- app/header.php
+++ app/header.php
@@ -1,33 +1,11 @@
-<div class="bg" style="display: none" typeof="sx:sx.Background">
-    <img property="imageEl" src="<?php header_image(); ?>"
-         height="<?php echo get_custom_header()->height; ?>"
-         width="<?php echo get_custom_header()->width; ?>" alt="" />
-</div>
-
-<header class="row" role="banner">
-    <div class="small-12 large-8 xlarge-9 columns">
-        <a href="/" title="Anne bloggt Cardamonchai ... Homepage">
-            <hgroup>
-                <h3>Anne bloggt</h3>
-
-                <h1><?php bloginfo('name') ?></h1>
-
-                <h2><?php bloginfo('description') ?></h2>
-            </hgroup>
-        </a>
-
-        <nav class="nav-main" role="navigation">
-            <?php c_create_navigation('header-menu'); ?>
-        </nav>
-    </div>
-
-    <div class="small-12 large-4 xlarge-3 columns">
-        <nav class="nav-meta hide-for-medium-down">
-            <?php c_create_navigation('meta-menu'); ?>
-        </nav>
-        <div class="search">
-            <?php get_search_form( true );?>
-        </div>
-    </div>
-
+<header>
+    <nav class="row textcenter">
+        <?php c_create_navigation('header-menu'); ?>
+    </nav>
+    <a href="/" title="Anne bloggt Cardamonchai ... Homepage">
+        <hgroup>
+            <h1><?php bloginfo('name') ?></h1>
+            <h2><?php bloginfo('description') ?></h2>
+        </hgroup>
+    </a>
 </header>
--- index.php
+++ index.php
@@ -7,16 +7,6 @@
         <section class="<?php if (is_home()): ?>home<?php endif;?> small-12 large-8 xlarge-9 columns">
             <?php get_template_part('app/loop'); ?>
         </section>
-        <aside class="small-12 large-4 xlarge-3 columns">
-            <?php if (is_page()): ?>
-                <?php get_template_part('app/area', 'page'); ?>
-            <?php elseif (is_single()): ?>
-                <?php get_template_part('app/area', 'single'); ?>
-            <?php else: ?>
-                <?php get_template_part('app/area', 'aside'); ?>
-            <?php endif; ?>
-        </aside>
-
     <?php endif; ?>
 </main>
 
