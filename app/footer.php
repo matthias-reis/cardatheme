c03a95dba125a5a2dffe5b36989ba52e0cd4c094
Matthias Reis <matthias.reis@xing.com>
2016-05-04 19:16:19 +0200

    removed easteregg from footer

--- app/footer.php
+++ app/footer.php
@@ -2,7 +2,7 @@
     <?php get_template_part('app/area', 'footer');?>
 
     <p class="copyright">
-        &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?><a href="/wp-content/themes/cardatheme/easteregg.html" rel="nofollow">.</a>
+        &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.
     </p>
 
 </footer>
